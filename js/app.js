d0561aeeb48d10ba0af1d312920926cc8916f741
Kiyoshi Murata <13k@linhareta.net>
2015-05-27 17:23:24 +0000

    Update email/google+ and page layout

---
+++ js/app.js
@@ -0,0 +1,9 @@
+(function($) {
+  $(function() {
+    var encoded = "xozhengn@tznvy.pbz",
+        cipher = new CaesarCipher(),
+        decoded = cipher.decode(encoded, 13);
+        
+    $("#email").text(decoded);
+  });
+})(window.jQuery);
